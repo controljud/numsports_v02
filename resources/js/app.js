7abb5b49a2531c0242c8dc2abae52c262c0d2611
Isaias Santos - Unidade de Soluções e Sistemas TI <Isaias.Santos@unimedrio.com.br>
2021-06-26 19:21:58 -0300

    Eita

---
+++ resources/js/app.js
@@ -0,0 +1,14 @@
+require('./bootstrap');
+
+import Vue from 'vue'
+import axios from 'axios'
+window.axios = axios
+
+import router from './router'
+import App from './components/App.vue'
+
+Vue.component('app', App)
+
+const app = new Vue({
+    router
+})
---
+++ resources/js/components/home/Home.vue
@@ -0,0 +1,11 @@
+<template>
+	<div>
+        Dinheiro
+	</div>
+</template>
+
+<script>
+	export default {
+
+	}
+</script>
